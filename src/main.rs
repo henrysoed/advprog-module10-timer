c7b6be518c2a43679903de322dbf2931e4c3fb75
Henrysoed <henry.soedibjo@gmail.com>
2024-05-05 23:12:24 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -108,6 +108,8 @@ fn main() {
         println!("Henry's Komputer: done!");
     });
 
+    println!("Henry's Komputer: hey hey!");
+
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
